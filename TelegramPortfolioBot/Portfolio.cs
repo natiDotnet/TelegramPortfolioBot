9ee380d83df22ab60808a1eafe540d207a037831
Natnael Yirga <NatiDotnet@gmail.com>
2025-01-13 14:40:08 +0000

    update profile

--- TelegramPortfolioBot/Portfolio.cs
+++ TelegramPortfolioBot/Portfolio.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Configuration;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
@@ -6,10 +7,13 @@ namespace TelegramPortfolio;
 public class Portfolio : IDisposable
 {
     private readonly TelegramBotClient bot;
+    private readonly IConfiguration config;
 
-    public Portfolio(TelegramBotClient bot)
+    public Portfolio(TelegramBotClient bot, IConfiguration config)
     {
         this.bot = bot;
+        this.config = config;
+        bot.OnMessage += OnMessage;
     }
 
     public void Dispose()
@@ -22,22 +26,27 @@ public class Portfolio : IDisposable
         switch (msg.Text)
         {
             case "/start":
-                Console.WriteLine("start...");
-
                 var replyMarkup = GetMenu();
                 await bot.SendMessage(msg.Chat, "Welcome to my portfolio bot! Use.",
                     replyMarkup: replyMarkup);
                 break;
             case "/about" or "Profile":
-                Console.WriteLine("about message");
+            var profile = 
+            """
+            Name: <b>Natnael Yirga</b> <b style='background-color: #5c2d91; color: white; padding: 5px; border-radius: 5px;'>.NET</b>
+            <b>LinkedIn: </b><i><a href='https://www.linkedin.com/in/natidotnet'>@natidotnet</a></i>
+            <b>X: </b><a href='https://x.com/natidotnet'>@natidotnet</a>
+            <b>Telegram: </b><a href='https://t.me/natidotnet'>@natidotnet</a>
+            <b>Github: </b><a href='https://github.com/natiDotnet'>@natidotnet</a>
 
+            """;
                 await bot.SendPhoto(
                     msg.Chat,
-                    "https://telegrambots.github.io/book/docs/photo-ara.jpg",
-                    "<b>Natnael Yirga</b>", ParseMode.Html);
+                    config["Image"]!,
+                    profile
+                    , ParseMode.Html);
                 break;
             case "/skills" or "Skills":
-            Console.WriteLine("Skills click");
                 var inlineKeys = GetSkills();
                 await bot.SendMessage(msg.Chat, "- - - - - - - - - - My Skills - - - - - - - - - -", replyMarkup: inlineKeys);
                 break;
@@ -71,18 +80,18 @@ public class Portfolio : IDisposable
     }
     private InlineKeyboardMarkup GetSkills()
     {
-        return new InlineKeyboardMarkup()
-                .AddButton("C#", "csharp")
-                .AddButton("Typescript", "ts")
-                .AddButton("Java", "java")
-                .AddNewRow()
-                .AddButton(".NET", "dotnet")
-                .AddButton("ASP.NET","asp")
-                .AddButton("Angular","ng")
-                .AddNewRow()
-                .AddButton("SQL","sql")
-                .AddButton("MySQL","mysql")
-                .AddButton("EF Core", "ef");
+        var skills = new InlineKeyboardMarkup();
+        int index = 0;
+        foreach (string skill in config.GetSection("Skills").Get<string[]>() ?? [] )
+        {
+            skills.AddButton(skill, skill);
+            if (index % 3 == 0)
+            {
+                skills.AddNewRow();
+            }
+            index++;
+        }
+        return skills;
     }
     private InlineKeyboardMarkup GetProjects()
     {
--- TelegramPortfolioBot/Program.cs
+++ TelegramPortfolioBot/Program.cs
@@ -2,11 +2,10 @@
 using Telegram.Bot;
 using TelegramPortfolio;
 using Microsoft.Extensions.Configuration;
-var builder = new ConfigurationBuilder()
+IConfiguration builder = new ConfigurationBuilder()
               .SetBasePath(Directory.GetCurrentDirectory())
               .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
               .Build();
 var bot = new TelegramBotClient(builder["Token"]!);
-var portfolio = new Portfolio(bot);
-bot.OnMessage += portfolio.OnMessage;
+var portfolio = new Portfolio(bot, builder);
 Console.ReadLine();
